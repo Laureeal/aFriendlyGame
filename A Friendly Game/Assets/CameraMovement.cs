663f0c56e7149f7df91a28a8cccff105a1fea5e1
Laureeal <godet.laure@gmail.com>
2016-12-03 15:14:31 +0100

    editScriptMovements

---
+++ A Friendly Game/Assets/CameraMovement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    public float speed = 1;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(transform.position.x + Input.GetAxis("Horizontal") * speed * Time.deltaTime, transform.position.y, transform.position.z);
+    }
+}
---
+++ A Friendly Game/Assets/Scripts/Wrap.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wrap : MonoBehaviour {
+    public float leftBorder = -51.2f;
+    public float rightBorder = 51.2f;
+    public bool copy = true;
+    float width;
+
+    void Start () {
+        width = rightBorder - leftBorder;
+        if (copy){
+            GameObject left = Instantiate<GameObject>(gameObject, new Vector3(transform.position.x - width, transform.position.y, transform.position.z),transform.rotation);
+            GameObject right = Instantiate<GameObject>(gameObject, new Vector3(transform.position.x + width, transform.position.y, transform.position.z), transform.rotation);
+            left.transform.SetParent(transform);
+            right.transform.SetParent(transform);
+            Destroy(left.GetComponent<Wrap>());
+            Destroy(right.GetComponent<Wrap>());
+
+        }
+    }
+	
+	void Update ()
+    {
+        
+        if (transform.position.x < leftBorder)
+        {
+            transform.position = new Vector3(transform.position.x +width, transform.position.y, transform.position.z) ;
+
+        }
+        if (transform.position.x > rightBorder)
+        {
+            transform.position = new Vector3(transform.position.x - width, transform.position.y, transform.position.z);
+
+        }
+
+        
+    }
+}
